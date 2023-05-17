e10947d67680d244b0214fda4cfe09d14ed2cd4e
Benjamin Loper <78445007+Loper1106@users.noreply.github.com>
2023-05-17 14:13:18 -0400

    Piggame

--- JavaScript/06-Modal/script.js
+++ JavaScript/06-Modal/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function addModal(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+function removeModal(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+    console.log(btnsOpenModal[i].addEventListener
+    ('click', addModal));
+
+btnCloseModal.addEventListener('click', removeModal);
+
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Escape'){
+        removeModal();
+    }
+});
